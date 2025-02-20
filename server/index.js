9cfafee4ccd4dcfe2432cc98e08f7da1e0f6fac8
surya <swainsuryakanta97@gmail.com>
2025-02-21 02:37:37 +0530

    update server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express, { json } from "express"
+import path from "path"
 import cookieParser from "cookie-parser"
 import { connectDB } from "./lib/connectDB.js"
 import userRouters from "./routes/user.route.js"
@@ -9,6 +10,7 @@ import paymentRouter from "./routes/paymet.route.js"
 
 const app = express()
 const port = process.env.PORT || 4000
+const __dirname = path.resolve()
 
 app.use(cors({
     origin : "https://ai-saas-text-to-image.onrender.com",
@@ -24,8 +26,12 @@ app.use("/user", userRouters)
 app.use("/image", imageRouter)
 app.use("/buy",paymentRouter)
 
-app.get("/",(req,res) => {
-    res.send("All set")
-})
+if(process.env.NODE_ENV == "production") {
+    app.use(express.static(path.join(__dirname,"/client/dist")))
+
+    app.get("*",(req , res) => {
+        res.sendFile(path.resolve(__dirname, "client" , "dist", "index.html"))
+    })
+}
 
 app.listen(port,()=> console.log("Server is running on ",port));
