50535034c8d305a45140796205dc032a6a34efb7
surya <swainsuryakanta97@gmail.com>
2025-02-21 01:51:29 +0530

    server url added

--- client/src/lib/axiosInstance.js
+++ client/src/lib/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL : "http://localhost:3000",
+    baseURL : "https://ai-saas-text-to-image-server.onrender.com",
     headers : {
         "Content-Type" : "application/json"
     },
