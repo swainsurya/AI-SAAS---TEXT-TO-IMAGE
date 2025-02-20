b6b13097345a3499c9b6d481bdc073044df8bfe8
surya <swainsuryakanta97@gmail.com>
2025-02-21 01:54:24 +0530

    client updated

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express()
 const port = process.env.PORT || 4000
 
 app.use(cors({
-    origin : "http://localhost:5173",
+    origin : "https://ai-saas-text-to-image.onrender.com",
     credentials : true
 }))
 
